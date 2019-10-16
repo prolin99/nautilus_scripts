70a250eb38099b95ea86de3281838a5f34cbba9f
prolin99 <prolin99@gmail.com>
2019-10-16 12:35:31 +0800

    目錄問題

--- unzip-big5.sh
+++ unzip-big5.sh
@@ -3,5 +3,6 @@
 file="$1"
 zip_path=`echo $file | sed 's/.zip//gi'`
 
-LANG=zh_TW.Big5 7z   e $file  -o$zip_path
-convmv --notest  -f big5 -t utf8 $zip_path/*
+#LANG=zh_TW.Big5 7z   e $file  -o$zip_path
+LANG=C 7z x $file -o$zip_path
+convmv --notest  -r -f big5 -t utf8 $zip_path/*
